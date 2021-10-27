cdd0bfbfee2eb64f4d19ea9cfd9862c5a2ec8b3b
Canouze <92824987+Canouze@users.noreply.github.com>
2021-10-27 20:22:35 +0100

    fourth function numbers between two user inputs

--- Loop.java
+++ Loop.java
@@ -1,7 +1,10 @@
+import javax.swing.JOptionPane;
 public class Loop{
 	public static void main(String[] args){
 		//declare vars
 		int i;
+		int start;
+		int stop;
 		//print all numbers between 1 and 100
 		for(i=1; i<=100; i=i+1){
 			System.out.println(i);
@@ -12,11 +15,28 @@ public class Loop{
 			System.out.println(i);
 		}
 		System.out.println("\n\n\n\n\n");
-			//print even numbers from negative to positive 10
-			for(i=-10; i<=10; i=i+1){
-				if(i%2==0){
-					System.out.println(i);
-				}
+		//print even numbers from negative to positive 10
+		for(i=-10; i<=10; i=i+1){
+			if(i%2==0){
+				System.out.println(i);
+			}
+		}
+		System.out.println("\n\n\n\n\n");
+		//take in two numbers and print numbers between
+		start=Integer.parseInt(JOptionPane.showInputDialog(null, "Enter first number"));
+		stop=Integer.parseInt(JOptionPane.showInputDialog(null, "Enter second number"));
+		if(start<stop){
+			for(i=start; i<=stop; i=i+1){
+				System.out.println(i);
+			}
+		}
+		else if(start>stop){
+			for(i=start; i>=stop; i=i-1){
+				System.out.println(i);
+			}
+		}
+		else{
+			System.out.println("Those numbers are the same!");
 		}
 	}
 }

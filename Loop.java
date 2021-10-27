bc935970010bed173b1a18db3b2e4f47e8ea0a4d
Canouze <92824987+Canouze@users.noreply.github.com>
2021-10-27 20:07:27 +0100

    Third function even numbers between puls minus 10

--- Loop.java
+++ Loop.java
@@ -2,14 +2,21 @@ public class Loop{
 	public static void main(String[] args){
 		//declare vars
 		int i;
-		//compute print all numbers between 1 and 100
+		//print all numbers between 1 and 100
 		for(i=1; i<=100; i=i+1){
 			System.out.println(i);
 		}
 		System.out.println("\n\n\n\n\n");
-		//compute print all numbers from 100 to 0
+		//print all numbers from 100 to 0
 		for(i=100; i>=0; i=i-1){
 			System.out.println(i);
+		}
+		System.out.println("\n\n\n\n\n");
+			//print even numbers from negative to positive 10
+			for(i=-10; i<=10; i=i+1){
+				if(i%2==0){
+					System.out.println(i);
+				}
 		}
 	}
 }

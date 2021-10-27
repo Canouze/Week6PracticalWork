63652300ce8fb3f45a560e2cd12fef8c35184dd2
Canouze <92824987+Canouze@users.noreply.github.com>
2021-10-27 19:55:14 +0100

    First function priint all numbers between 1 and 100

---
+++ Loop.java
@@ -0,0 +1,10 @@
+public class Loop{
+	public static void main(String[] args){
+		//declare vars
+		int i;
+		//compute print all numbers between 1 and 100
+		for(i=1; i<=100; i=i+1){
+			System.out.println(i);
+		}
+	}
+}
